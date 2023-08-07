9d36cc440dead2e6ece45205eed9fd547500c6c4
ykw0104 <ykw0104@163.com>
2023-08-07 15:13:40 +0800

    mock引入

--- vite.config.ts
+++ vite.config.ts
@@ -1,36 +1,62 @@
-import { defineConfig } from "vite";
+// https://vitejs.dev/config/
+import { defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
 import path from "path";
-
-// 引入svg需要的插件
+//引入svg需要用到插件
 import { createSvgIconsPlugin } from "vite-plugin-svg-icons";
+//mock插件提供方法
+import { viteMockServe } from "vite-plugin-mock";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
+export default defineConfig(({ command, mode }) => {
+  //获取各种环境下的对应的变量
+  const env = loadEnv(mode, process.cwd());
 
-    createSvgIconsPlugin({
-      // Specify the icon folder to be cached
-      iconDirs: [path.resolve(process.cwd(), "src/assets/icons")],
-      // Specify symbolId format
-      symbolId: "icon-[dir]-[name]",
-    }),
-  ],
+  return {
+    publicPath: "https://gitee.com/jch1011/guiguzhenxuan",
 
-  resolve: {
-    alias: {
-      "@": path.resolve("./src"), // 相对路径别名配置，使用 @ 代替 src
-    },
-  },
+    plugins: [
+      vue(),
 
-  css: {
-    preprocessorOptions: {
-      // 配置scss的全局变量
-      scss: {
-        javascriptEnabled: true,
-        additionalData: '@import "./src/styles/variable.scss";',
+      // svg插件的设置
+      createSvgIconsPlugin({
+        iconDirs: [path.resolve(process.cwd(), "src/assets/icons")],
+        symbolId: "icon-[dir]-[name]",
+      }),
+
+      // mock的设置
+      viteMockServe({
+        mockPath: "mock",
+        localEnabled: command === "serve", //保证开发阶段可以使用mock接口
+      }),
+    ],
+
+    resolve: {
+      alias: {
+        "@": path.resolve("./src"), // 相对路径别名配置，使用 @ 代替 src
       },
     },
-  },
+
+    css: {
+      preprocessorOptions: {
+        //scss全局变量的配置
+        scss: {
+          javascriptEnabled: true,
+          additionalData: '@import "./src/styles/variable.scss";',
+        },
+      },
+    },
+    //代理跨域
+    server: {
+      proxy: {
+        [env.VITE_APP_BASE_API]: {
+          //获取数据的服务器地址设置
+          target: env.VITE_SERVE,
+          //需要代理跨域
+          changeOrigin: true,
+          //路径重写
+          rewrite: (path) => path.replace(/^\/api/, ""),
+        },
+      },
+    },
+  };
 });
