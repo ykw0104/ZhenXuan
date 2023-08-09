75269e04c797afb50bea28074ebde2ecc7aeb8d4
ykw0104 <ykw0104@163.com>
2023-08-09 16:50:59 +0800

    layout静态页面布局

---
+++ src/layout/index.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="layout_container">
+    <!-- 左侧菜单 -->
+    <div class="layout_slider">1111</div>
+
+    <!-- 顶部导航 -->
+    <div class="layout_tabbar">2222</div>
+
+    <!-- 主体 -->
+    <div class="layout_main">333</div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss">
+.layout_container {
+  width: 100%;
+  height: 100vh;
+
+  .layout_slider {
+    width: $base-menu-width;
+    height: 100vh;
+    background-color: $base-menu-background;
+  }
+
+  .layout_tabbar {
+    position: fixed;
+    top: 0;
+    left: $base-menu-width;
+    width: calc(100% - $base-menu-width);
+    height: $base-tabbar-height;
+  }
+
+  .layout_main {
+    position: fixed;
+    left: $base-menu-width;
+    top: $base-tabbar-height;
+    width: calc(100% - $base-menu-width);
+    height: calc(100vh - $base-tabbar-height);
+    padding: 20px;
+    overflow: auto;
+  }
+}
+</style>
--- src/router/routes.ts
+++ src/router/routes.ts
@@ -10,7 +10,7 @@ export const constantRoute = [
   {
     path: "/",
     name: "layout",
-    component: () => import("@/views/home/index.vue"),
+    component: () => import("@/layout/index.vue"),
   },
 
   // 404
