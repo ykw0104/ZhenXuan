ea00dec3277282dc1aa770f50bbce17a483b2ee2
ykw0104 <ykw0104@163.com>
2023-08-10 11:42:52 +0800

    logo组件

---
+++ src/layout/logo/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="logo">
+    <!-- <img src="../../../public/logo.jpeg" />
+    <p>Yomiya</p> -->
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss">
+.logo {
+  display: flex;
+  align-items: center;
+  width: 100%;
+  height: $base-menu-logo-height;
+  padding: 20px;
+  color: #fff;
+  text-align: center;
+
+  img {
+    width: 40px;
+    height: 40px;
+  }
+
+  p {
+    margin-left: 10px;
+    font-size: $base-logo-title-fontSize;
+  }
+}
+</style>
